7c8d08b8c6ba9c125cd30695996d707a754f8281
jaytsol <jaytsol@naver.com>
2025-07-07 04:05:02 +0900

    fixed edit page feature

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -36,7 +36,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          editUrl: 'https://github.com/jaytsol/surfai-docs/website/',
+          editUrl: 'https://github.com/jaytsol/surfai-docs/blob/main/website/',
         },
         blog: false, // Disable the blog plugin
         theme: {

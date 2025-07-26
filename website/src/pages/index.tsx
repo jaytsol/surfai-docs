90ea4de1602306962d0f996cdfd545d23f752a42
jaytsol <jaytsol@naver.com>
2025-07-27 01:50:07 +0900

    homepage fixed

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -21,23 +21,18 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/OVERVIEW">
+            to="/category/project-overview">
             <Translate id="default.overview.link.text">OVERVIEW</Translate>
           </Link>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/ARCHITECTURE">
+            to="/category/architecture">
             <Translate id="default.architecture.link.text">ARCHITECTURE</Translate>
           </Link>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/도구/이미지-비디오-생성/백엔드/API_SPEC">
-            <Translate id="default.backend.link.text">BACKEND</Translate>
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/도구/이미지-비디오-생성/프론트엔드/COMPONENTS">
-            <Translate id="default.frontend.link.text">FRONTEND</Translate>
+            to="/category/tools">
+            <Translate id="default.tools.link.text">TOOLS</Translate>
           </Link>
         </div>
       </div>
